8ec3d3adae6897b2d90a191ea9854af96542481c
Juan Alderete <juan41285@gmail.com>
2016-08-30 01:11:14 -0300

    fix direcciones

--- direcciones/app/scripts/controllers/AccionesCtrl.js
+++ direcciones/app/scripts/controllers/AccionesCtrl.js
@@ -7,10 +7,25 @@
     .controller('AccionesCtrl', AccionesCtrl);
 
 
-   function AccionesCtrl($scope) {
+   function AccionesCtrl($scope, $http) {
 
    
     $scope.template = {'acciones': 'templates/acciones.tpl.html'};
+
+    var _selected;
+
+  $scope.selected = undefined;
+  $scope.states = ['proyecto 1', 'proyecto 2', 'proyecto 33', 'Proyecto con la letra A', 'Poryecto con la letra S', 'Otro nomre de proyecto'];
+
+  $scope.colaborador = undefined;
+  $scope.colaboradores = ['Luis Santillan', 'Pedro Ruiz', 'Maria Lopez', 'Carlos Paz', 'Laura Monteros', 'Andres Chavez'];
+  $scope.integrantes = [];
+  $scope.AddIntegrante = function(col){
+  	console.log(col);
+  	$scope.integrantes.push(col);
+  	console.log($scope.colaborador);
+  	$scope.colaborador = undefined;
+  }
 }
 
 })();
