35a8a35858f9aad9e370f4df2d9053b7f5e0377e
Juan Alderete <juan41285@gmail.com>
2016-08-31 00:31:41 -0300

    Fix admin

--- admin/app/scripts/controllers/EstrategicosCtrl.js
+++ admin/app/scripts/controllers/EstrategicosCtrl.js
@@ -11,22 +11,26 @@
 
    
     $scope.template = {'estrategicos': 'templates/abm-estrategicos.tpl.html'};
-    $scope.texto = 'Si conectoa';
-    $scope.direccion = {};
-    $scope.guardarDireccion = function(){
-    	 $http({
+     var _selected;
 
-                    url: "http://rest.innovacioneducativa.gob.ar/ppe/fase0/direccion",
-                    data: $scope.direccion,
-                    method: 'POST',
-                    headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+  $scope.selected = {
+    'nombre':'',
+    'des':''
+  };
+  $scope.des = false;
+  $scope.indicador = {};
+  $scope.indicadores =[];
+  $scope.AddIndicador = function(indicador){
+    var snew  = {};
+    var snew = indicador;
+   $scope.indicadores.push(snew);
+    console.log($scope.indicadores);
+     $scope.indicador = {};
 
-                }).success(function(data){
 
-                    console.log("OK", data)
-
-                }).error(function(err){"ERR", console.log(err)})
-    }
+  }
+   $scope.selected.des = 'Los objetivos operativos son los puntos de referencia de los proyectos diarios, semanales o mensuales que implementan grandes objetivos estratégicos. Los objetivos operativos, también llamados objetivos tácticos, se establecen con los objetivos estratégicos en mente y proporcionan un medio para la gestión y el personal para romper un objetivo estratégico más amplio en tareas realizables. Por ejemplo, la consecución del objetivo estratégico de aumentar en un 25 por ciento en los ingresos por ventas requiere la realización de los objetivos operativos para desarrollar y ejecutar una estrategia de publicidad efectiva, junto con otros objetivos operacionales. Al igual que con los objetivos estratégicos, los objetivos operativos también deben ser medibles y específicos, aunque su enfoque es más estrecho.';
+  $scope.states = ['Obejtivo operativo 1', 'Obejtivo operativo 2', 'Obejtivo operativo 33', 'Obejtivo operativo con la letra A', 'Obejtivo operativo con la letra S', 'Otro nomre de Obejtivo operativo'];
 }
 
 })();
